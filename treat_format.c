f5c47baf1c3c10ffcd0df7076e9884e089c29ef6
Enyeobi Bon <BoncodeX@gmail.com>
2022-11-21 17:41:10 -0800

    treat_format.c

--- treat_format.c
+++ treat_format.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
@@ -8,25 +8,24 @@
  *
  * Return: pointer to a function or NULL if not found
  */
-
 char *(*get_print_func(char c))(modifier_t *, va_list)
 {
 	int i;
 	t_print t[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'i', print_int},
-		{'d', print_int},
-		{'u', print_unsigned_int},
-		{'o', print_octal},
-		{'x', print_hex},
-		{'X', print_hex},
-		{'b', print_binary},
-		{'S', print_big_s},
-		{'p', print_pointer},
-		{'r', print_rev},
-		{'R', print_rot},
-		{'\0', NULL}};
+	    {'c', print_char},
+	    {'s', print_string},
+	    {'i', print_int},
+	    {'d', print_int},
+	    {'u', print_unsigned_int},
+	    {'o', print_octal},
+	    {'x', print_hex},
+	    {'X', print_hex},
+	    {'b', print_binary},
+	    {'S', print_big_s},
+	    {'p', print_pointer},
+	    {'r', print_rev},
+	     {'R', print_rot},
+	    {'\0', NULL}};
 	for (i = 0; t[i].f; i++)
 	{
 		if (t[i].f == c)
