58370305be33c844be5d69ae578b1bdbc14bb59e
Enyeobi Bon <BoncodeX@gmail.com>
2022-11-21 17:54:45 -0800

    Modified get_modifier.c

--- main.c
+++ main.c
@@ -13,8 +13,6 @@ int main(void)
     int len2;
     unsigned int ui;
     void *addr;
-    char aab[] = "slen";
-    char a;
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
@@ -40,7 +38,7 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n", aab);
-    printf("Unknown:[%s]\n", aab);
+    _printf("Unknown:[%r]\n");
+
     return (0);
 }
